7e16cdfc782e0f9fad3be8b63fbbe19feba6d1d6
Renan Viana <renanvianalc@gmail.com>
2018-09-08 18:14:22 -0300

    Configuração do backend

---
+++ src/main/java/br/com/mysistems/mysistems/MysistemsApplication.java
@@ -0,0 +1,12 @@
+package br.com.mysistems.mysistems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysistemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysistemsApplication.class, args);
+	}
+}
